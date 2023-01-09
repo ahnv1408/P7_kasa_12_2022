57e1e59c2f5e947bd9d37470d405a39da5790a23
Alicia <alicia2.haumann@gmail.com>
2023-01-09 17:43:51 +0100

    id commit

--- src/components/Tags.jsx
+++ src/components/Tags.jsx
@@ -2,39 +2,22 @@ import '../style/tags.css';
 import React,{useState, useEffect} from "react";
 import axios from "axios";
 
-function Tags(props) {
-    console.log(props.productsList[0].tags);
+function Tags({product}) {
+    console.log({product});
     return (
-        <div className="tags">
-            {
-                props.productsList.map((product) =>
-                <>
-                <h1 className="lodgment_name">{product.title}</h1>
-                <p className="lodgment_location">{product.location}</p>
-                <ul className='list_tags'>
-                    <li className="lodgment_tag" key={product.tags}>{product.tags}</li>           
-                </ul>
-                </>)
-            }
-        </div>
+        <div className="tags">      
+           
+            <>
+            <h1 className="lodgment_name">{product.title}</h1>
+            <p className="lodgment_location">{product.location}</p>
+            <ul className='list_tags'>
+                <li className="lodgment_tag" key={product.tags}>{product.tags}</li>           
+            </ul>
+            </>        
+    </div>
     )
-    // const tags = props.tags;
-    // return(
-    //     <ul className='tags'>
-    //         {
-    //             tags.map((tag) =>
-    //             <li className='tag' key={tag.tags}>{tag.tags}</li> )
-    //         }
-    //     </ul>
-    // )
+ 
 }
 
 export default Tags;
 
-        
-        // <ul className='tags'>
-        //     {
-        //         productsList.map((product) =>
-        //         <li className='tag' key={product.tags}>{product.tags}</li> )
-        //     }
-        // </ul>
--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,63 +1,58 @@
 import React from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import '../style/logement.css';
 import NotFound from '../pages/NotFound'
 import Header from '../components/Header';
 import Tags from "../components/Tags";
 import Rate from "../components/Rate";
 import Host from "../components/Host";
+import productsList from "../products/products.json";
 import Caroussel from '../components/Caroussel/Caroussel';
-import productsList from '../products/products.json';
-import {useState, useEffect} from "react";
-import axios from "axios";
+
 
 
 function Logement () {
-    // const { productId } = useParams();
-    // const product = productsList.find((product) => product.id === productId);
-    // const { title, location, pictures, rating, host, equipments, description } = product;
+
+    const { id } = useParams();
+    console.log(id);
+    // let chaine = id.toString();
+    // console.log(chaine);
+    // let id1 = parseInt(chaine.substring(1, 9));
+
+    // let myFunc = num => Number(num);
+      
+    // var intArr = Array.from(String(id), myFunc);
+    // for (let index = 1; index < intArr.length; index++) {
+    //     const element = intArr[index];
+    //     console.log(element);
+    // }
    
-   
-   
-    const [product, setProduct] = useState([]);
-    const url = 'http://localhost:3000/products/products.json'
-    useEffect(()=>{
-        axios.get(`${url}`)
-        .then(response => setProduct(response.data))
-        .catch(err => console.log(err))
-      },[]);
-   const lodgmentId = window.location.pathname.substring(10);
-   const lodgment = product.find((item) => item.id === lodgmentId);
-//    if(!lodgment) return <NotFound/>
+ 
+    const product = productsList.find((p) =>  {console.log(":" + p.id); return (":" + p.id) == (id);
+    });
+
+    console.log(product);
+
 
     return (
-        <>
-            <Header />
-       
-
-
-
-
-
-            {/* <section>
-                <Caroussel props={pictures}/>
-                <div className="info">
-                    <h1>{title}</h1>
-                </div>
-            </section> */}
-
-
-
-
-
-           
-            <Caroussel productsList={productsList}/>
         
-            
-            <Host productsList={productsList}/>
-            <Tags productsList={productsList}/>
-            <Rate productsList={productsList} />
-        </>
+        <>         
+         
+       
+            <Header />           
+             {/* <Caroussel product={product}/>     */}
+            <div className="tags_host_rate">
+                <div className="tags">
+                    <Tags product={product}/>
+                </div>
+                <div className="host_rate">
+                    <Host product={product}/>            
+                    <Rate product={product}/> 
+                </div>
+            </div>
+
+            </>
+      
     )
 }
 export default Logement;
