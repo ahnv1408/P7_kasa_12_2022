67b523c4dffdbfdc884daf80ea038dfd35613a83
Alicia <alicia2.haumann@gmail.com>
2023-01-11 17:06:57 +0100

    final commit

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -2,31 +2,21 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import Desc from '../components/Desc';
+import Equipments from "./Equipments";
 import '../style/description.css';
 
 
 function Description({product}) {
-        
+        console.log(product.equipments);
     const [ visible, setVisible ] = useState (false);
     const display=() => {
         setVisible(!visible);
     }
     return (
-    <div className="collapses">          
         <>
-            <section className="collapse_style_des">
-                <div className="collapse_title_log">                                              
-                    <h2 className="title_log">Description</h2>                                               
-                                        
-                    <p className="icon" onClick={display}>{visible?<FontAwesomeIcon icon={faChevronUp} />:<FontAwesomeIcon icon={faChevronDown} />}</p>
-                     
-                </div>
-                <div className="collapse_description_log">
-                    <div className="list_description">
-                    {visible && <p className="info_description_log">{product.description}</p>}
-                    </div>
-                </div>
-            </section>
+    <div className="collapses">   
+        <Desc product={product}/>      
             <section className="collapse_style_equi">
                 <div className="collapse_title_log">                                              
                     <h2 className="title_log">Equipements</h2>                                               
@@ -36,17 +26,14 @@ function Description({product}) {
                 </div>
                 <div className="collapse_description_log">
                     <ul className="list_equipment">
-                        {visible && <li className="info_description_log_equip">{product.equipments[0]}</li>}
-                        {visible && <li className="info_description_log_equip">{product.equipments[1]}</li>}
-                        {visible && <li className="info_description_log_equip">{product.equipments[3]}</li>}
-                        {visible && <li className="info_description_log_equip">{product.equipments[4]}</li>}
-                        {visible && <li className="info_description_log_equip">{product.equipments[5]}</li>}
+                        {product.equipments.map((equip =>                        
+                            visible &&<li className="info_description_log_equip">{equip}</li>
+                        ))}
                     </ul>
                 </div>
             </section>
-        </>       
-    
-</div>
+    </div>
+    </>
     )
 }
 
